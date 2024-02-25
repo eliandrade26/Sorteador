6b83474e9683dec6ceb6f6b79836316e198937e2
eliandrade26 <eli_carlos_14@hotmail.com>
2024-02-25 19:04:39 -0300

    Atualizado 25/02

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function generateNumber(){
-    
-    const min=Math.ceil (document.querySelector(".input-min").value) // Vinculando a variavel min ao input-min do html e buscando apenas o valor
-    const max=Math.floor(document.querySelector(".input-max").value)
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;//
-  
-    alert(result)
+const generateNumber = document.querySelector(".generateNumber");
+
+
+generateNumber.addEventListener("click", generate);
+
+function generate() {
+    const min = Math.ceil(document.querySelector(".input-min").value);
+    const max = Math.floor(document.querySelector(".input-max").value);
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+
+    // Atualiza o conteúdo da classe "numberview" com o número gerado
+    document.querySelector(".number-view").textContent = result;
 }
+
